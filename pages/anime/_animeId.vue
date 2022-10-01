0b7b0a99483704dc7c5e75a341e28cc96d0f1945
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2022-10-01 12:02:07 +0900

    feat: animeId.vueを追加

---
+++ pages/anime/_animeId.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>_animeId.vue</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'nuxt-property-decorator'
+
+@Component
+export default class AnimeId extends Vue {}
+</script>
