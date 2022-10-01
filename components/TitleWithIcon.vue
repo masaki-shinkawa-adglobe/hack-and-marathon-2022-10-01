aabcdaba1910d4b2f2181002cd386c5874c898a1
mingue.kim <mingue.kim@adglobe.co.jp>
2022-10-01 14:55:16 +0900

    aタグ追加

--- components/TitleWithIcon.vue
+++ components/TitleWithIcon.vue
@@ -11,15 +11,15 @@
   <div class="mt-2 px-4">
     <div class="flex mx-2">
       <img class="w-6 mx-2 mb-2" src="~/assets/image/webPage.svg">
-      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">公式HP</h1>
+      <a :href="officialUrl" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">公式HP</a>
     </div>
     <div class="flex mx-2 mb-2">
       <img class="w-6 mx-2" src="~/assets/image/fashion.svg">
-      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">衣装購入はこちら</h1>
+      <a :href="shop" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">衣装購入はこちら</a>
     </div>
     <div class="flex mx-2 mb-2">
       <img class="w-6 mx-2" src="~/assets/image/Parthenon.svg">
-      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">アニメの聖地を地図で見る</h1>
+      <a :href="location" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">アニメの聖地を地図で見る</a>
     </div>
   </div>
 
@@ -30,5 +30,20 @@
 import { Component, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class TitleWithIcon extends Vue {}
+export default class TitleWithIcon extends Vue {
+  get officialUrl() {
+    // TODO: APIのレスポンスを返す
+    return "https://lycoris-recoil.com/"
+  }
+  get shop() {
+    // TODO: APIのレスポンスを返す
+    return "https://www.costowns.com/search.html?k=test"
+  }
+  get location() {
+    // TODO: APIのレスポンスを返す
+    return "https://www.google.co.jp/maps/place/%E9%9D%99%E5%B2%A1%E7%9C%8C%E9%9D%99%E5%B2%A1%E5%B8%82"
+  }
+
+
+}
 </script>
