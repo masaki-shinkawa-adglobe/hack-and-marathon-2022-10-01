b1a7dba439e0975fb926aceefaa9e5787b42d40a
mingue.kim <mingue.kim@adglobe.co.jp>
2022-10-01 14:26:13 +0900

    詳細タイトル実装

--- components/TitleWithIcon.vue
+++ components/TitleWithIcon.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>TitleWithIcon</div>
+<div class="container">
+
+<!-- header -->
+<div class="flex mx-2 border-b border-pink-500 pb-1">
+    <img class="w-8 mx-2" src="~/assets/image/info.svg">
+    <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 24px;">アニメ情報</h1>
+  </div>
+
+<!-- Link Items -->
+  <div class="mt-2 px-4">
+    <div class="flex mx-2">
+      <img class="w-6 mx-2 mb-2" src="~/assets/image/webPage.svg">
+      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">公式HP</h1>
+    </div>
+    <div class="flex mx-2 mb-2">
+      <img class="w-6 mx-2" src="~/assets/image/fashion.svg">
+      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">衣装購入はこちら</h1>
+    </div>
+    <div class="flex mx-2 mb-2">
+      <img class="w-6 mx-2" src="~/assets/image/Parthenon.svg">
+      <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">アニメの聖地を地図で見る</h1>
+    </div>
+  </div>
+
+</div>
 </template>
 
 <script lang="ts">
