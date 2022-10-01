3f31e457fc7ba632432930f850eb33f9c0f9473a
Masaki-Shinkawa <masaki.shinkawa@adglobe.co.jp>
2022-10-01 15:14:52 +0900

    feat: colorsを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,14 +6,16 @@ module.exports = {
     './plugins/**/*.{js,ts}',
     './nuxt.config.{js,ts}',
   ],
+  mode: 'jit',
   // darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       rotate: {
         20: '20deg',
       },
-      backgroundColor: {
+      colors: {
         'pink-primary': '#EF5DA8',
+        'pink-secondary': '#FCDDEC',
       },
     },
   },
