c1bfe40f03fd9c4e606b34c4c1d7ca42be8cfce5
mingue.kim <mingue.kim@adglobe.co.jp>
2022-10-01 17:55:10 +0900

    詳細画面実装

--- components/AnimeZoomImageList.vue
+++ components/AnimeZoomImageList.vue
@@ -1,10 +1,30 @@
 <template>
-  <div>AnimeZoomImageList</div>
+  <div>
+    <div class="mx-2 flex border-b border-pink-primary pb-1">
+      <img class="mx-2 w-8" src="~/assets/image/cracker.svg" />
+      <h1
+        class="flex items-center font-bold text-pink-primary"
+        style="font-size: 24px"
+      >
+      みんなのコスプレ
+      </h1>
+    </div>
+    <ul class="m-2 grid h-[calc(100vw/3/2*3)] grid-cols-3 gap-2">
+      <AnimeZoomImage
+        v-for="(image, i) in images"
+        :key="i"
+        :image="image"
+      ></AnimeZoomImage>
+    </ul>
+  </div>
 </template>
 
 <script lang="ts">
-import { Component, Vue } from 'nuxt-property-decorator'
+import { Component, Prop, Vue } from 'nuxt-property-decorator'
 
 @Component
-export default class AnimeZoomImageList extends Vue {}
+export default class AnimeZoomImageList extends Vue {
+  @Prop({ default: [] })
+  images!: string[]
+}
 </script>
--- components/TitleWithIcon.vue
+++ components/TitleWithIcon.vue
@@ -1,25 +1,19 @@
 <template>
 <div class="container">
 
-<!-- header -->
-<div class="flex mx-2 border-b border-pink-500 pb-1">
-    <img class="w-8 mx-2" src="~/assets/image/info.svg">
-    <h1 class="items-center text-pink-500 flex font-bold" style="font-size: 24px;">アニメ情報</h1>
-  </div>
-
 <!-- Link Items -->
   <div class="mt-2 px-4">
     <div class="flex mx-2">
       <img class="w-6 mx-2 mb-2" src="~/assets/image/webPage.svg">
-      <a :href="officialUrl" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">公式HP</a>
+      <a :href="officialUrl" target="_blank" class="items-center text-pink-primary flex font-bold" style="font-size: 16px;">公式HP</a>
     </div>
     <div class="flex mx-2 mb-2">
       <img class="w-6 mx-2" src="~/assets/image/fashion.svg">
-      <a :href="shop" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">衣装購入はこちら</a>
+      <a :href="shop" target="_blank" class="items-center text-pink-primary flex font-bold" style="font-size: 16px;">衣装購入はこちら</a>
     </div>
     <div class="flex mx-2 mb-2">
       <img class="w-6 mx-2" src="~/assets/image/Parthenon.svg">
-      <a :href="location" target="_blank" class="items-center text-pink-500 flex font-bold" style="font-size: 16px;">アニメの聖地を地図で見る</a>
+      <a :href="location" target="_blank" class="items-center text-pink-primary flex font-bold" style="font-size: 16px;">アニメの聖地を地図で見る</a>
     </div>
   </div>
 
